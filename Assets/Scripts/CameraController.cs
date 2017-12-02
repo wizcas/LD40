8a18faf2d7ed928e581a56f7d4ccd82d19eed57d
Wizcas <chen@wizcas.me>
2017-12-02 15:49:40 +0800

    Added test model & sprites. Camera controlling

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿/*****************************************************
+/* Created by Wizcas Chen (http://wizcas.me)
+/* Please contact me if you have any question
+/* E-mail: chen@wizcas.me
+/* 2017 © All copyrights reserved by Wizcas Zhuo Chen
+*****************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+    [Range(0, 15)]
+    public float horizontalViewRange = 10;
+    [Range(0, 10)]
+    public float verticalViewRange = 5;
+    public float anglePerPx = .01f;
+
+    private void Update()
+    {
+        var screenCenterPos = Camera.main.ViewportToScreenPoint(new Vector3(.5f, .5f, 0));
+        var delta2Center = Input.mousePosition - screenCenterPos;
+        var xAngle = Mathf.Clamp(-delta2Center.y * anglePerPx, -verticalViewRange, verticalViewRange);
+        var yAngle = Mathf.Clamp(delta2Center.x * anglePerPx, -horizontalViewRange, horizontalViewRange);
+
+        var viewAngle = new Vector3(xAngle, yAngle, 0f);
+        var camRot = Quaternion.Euler(viewAngle);
+        Camera.main.transform.rotation = camRot;
+    }
+}
---
+++ Assets/Scripts/GuestManager.cs
@@ -0,0 +1,30 @@
+﻿/*****************************************************
+/* Created by Wizcas Chen (http://wizcas.me)
+/* Please contact me if you have any question
+/* E-mail: chen@wizcas.me
+/* 2017 © All copyrights reserved by Wizcas Zhuo Chen
+*****************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuestManager : MonoBehaviour 
+{
+    SpriteRenderer[] renderers;
+    Camera cam;
+
+    private void Awake()
+    {
+        renderers = this.GetComponentsInAllChildren<SpriteRenderer>();
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        foreach(var rdr in renderers)
+        {
+            rdr.transform.rotation = cam.transform.rotation;
+        }
+    }
+}
